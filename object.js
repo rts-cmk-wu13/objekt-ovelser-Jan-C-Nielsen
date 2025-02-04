bd7cb664d86baf1f88558b1e97696971eadae475
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-02-04 10:55:47 +0100

    Opg 2

--- object.js
+++ object.js
@@ -5,6 +5,20 @@ let person = {
     zip: 3320
 }
 
+let person2 = {
+    name: "Jan",
+    adress: "Bistruplund",
+    number: 8,
+    zip: 3320,
+    atHome: false,
+    stores: ["meny", "netto", "rema1000"],
+    transport: {
+        bus: true,
+        train: true
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
 console.log(person.name)
 console.log(person.adress)
@@ -15,5 +29,10 @@ console.log(person["name"])
 console.log(person["adress"])
 console.log(person["number"])
 console.log(person["zip"])
+
+person2.stores.forEach(x => console.log(x))
+console.log(person2.transport.bus)
+console.log(person2.transport.train)
+
 }
 )
